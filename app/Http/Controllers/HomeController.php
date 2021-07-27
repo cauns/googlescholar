7f673183c24e04c001c15b3c8bfba4f2643c4b81
cauns <cauns@abilive.vn>
2021-07-27 14:16:39 +0700

    fix output

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
     public function frontEnd()
     {
         $items = DB::table('author_cite_articles')
-            ->select('cites.cite_id','cites.link_get', 'authors.alias_name','author_cite_articles.total','authors.author_id')
+            ->select('cites.*', 'authors.alias_name','authors.author_id','author_cite_articles.total as x_total','author_cite_articles.total_only_author as x_total_only_author','author_cite_articles.total_not_by_author as x_total_not_by_author')
             ->leftJoin('authors', 'author_cite_articles.author_id', '=', 'authors.author_id')
             ->leftJoin('cites', 'cites.cite_id', '=', 'author_cite_articles.cite_id')
             ->get();
